1dd4772c0de888f2f1a07b92b97272af6fda8e2b
Ulisses Dias <ulisses@ft.unicamp.br>
2021-05-25 14:19:07 -0300

    Corrigindo erro no Put

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post(endpoint, (req, res) => {
 });
 
 app.put(`${endpoint}/:id`, (req, res) =>{
-    const id = req.params.id;
+    const id = parseInt(req.params.id);
     const note = {
         id : id,
         title : req.body["title"],
